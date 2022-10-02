cb30b982852899ddf1ed2f933be8be5d09e1a0da
Max Graman <me@maxg.xyz>
2022-10-02 19:24:36 -0400

    pre-router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from 'src/mocks/mock-heroes';
+import { HeroService } from '../hero.service';
 import { Hero } from '../interfaces/hero';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +10,24 @@ import { Hero } from '../interfaces/hero';
   styleUrls: ['./heroes.component.sass'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
-    console.dir(hero);
+    console.dir('Selected Hero: ' + hero);
+    this.messageService.add(`HeroesComponenet: Selected hero id =${hero.id}`);
   }
-  constructor() {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
-  ngOnInit(): void {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
